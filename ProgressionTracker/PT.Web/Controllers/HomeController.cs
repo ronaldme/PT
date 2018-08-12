9c7ef950f17ea9a29f449cb654b31e62e0e4933e
ronaldme <ronaldme@gmail.com>
2018-08-12 11:03:55 +0200

    Add web project

---
+++ ProgressionTracker/PT.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PT.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
