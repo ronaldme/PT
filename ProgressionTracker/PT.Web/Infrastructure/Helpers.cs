3a7951f9e6bde775de2d065f575f7ed4bcd98c69
ronaldme <ronaldme@gmail.com>
2018-08-18 08:48:36 +0200

    Add helpers

---
+++ ProgressionTracker/PT.Web/Infrastructure/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using X.PagedList;
+
+namespace PT.Web.Infrastructure
+{
+    public static class Helpers
+    {
+        public static async Task<IPagedList<TDestination>> ProjectToPagedListAsync<TDestination>(
+            this IQueryable queryable, IConfigurationProvider config, int pageNumber, int pageSize)
+        {
+            return await queryable.ProjectTo<TDestination>(config).ToPagedListAsync(pageNumber, pageSize);
+        }
+
+        public static SelectList ToSelectList(this Dictionary<int, string> dictionary)
+        {
+            return new SelectList(dictionary, "Key", "Value");
+        }
+    }
+}
