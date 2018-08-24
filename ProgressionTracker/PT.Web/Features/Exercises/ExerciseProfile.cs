eb5606fc5713abe35c90298f715509932e3c200c
ronaldme <ronaldme@gmail.com>
2018-08-24 19:16:41 +0200

    Add ExerciseProfile

---
+++ ProgressionTracker/PT.Web/Features/Exercises/ExerciseProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PT.DAL.Entities;
+
+namespace PT.Web.Features.Exercises
+{
+    public class ExerciseProfile : Profile
+    {
+        public ExerciseProfile()
+        {
+            CreateMap<Exercise, ExerciseViewModel>();
+            CreateMap<UpsertExerciseCommand, Exercise>();
+        }
+    }
+}
