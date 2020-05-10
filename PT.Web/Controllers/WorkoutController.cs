52ce9b0a381fa1ddd9db50552063322ee3416b92
Ronald <ronaldme@gmail.com>
2020-05-10 09:23:42 +0200

    Add WorkoutController

---
+++ PT.Web/Controllers/WorkoutController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PT.DAL;
+using PT.DAL.Entities;
+
+namespace PT.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WorkoutController : ControllerBase
+    {
+        private readonly PtDbContext _db;
+
+        public WorkoutController(PtDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public List<Workout> List() => _db.Workouts.ToList();
+
+        [HttpPost]
+        [Route("/workout/add")]
+        public async Task Add(AddWorkout item)
+        {
+            _db.Workouts.Add(new Workout
+            {
+                Name = item.Name,
+            });
+
+            await _db.SaveChangesAsync();
+        }
+
+        public class AddWorkout
+        {
+            public string Name { get; set; }
+        }
+    }
+}
