f4e1beab46874840ebca4309c8a8355cdb0a01ef
Ronald <ronaldme@gmail.com>
2020-05-12 19:42:16 +0200

    Add toggle, history methods in workout calender

--- PT.Web/Controllers/WorkoutCalenderController.cs
+++ PT.Web/Controllers/WorkoutCalenderController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PT.DAL;
@@ -21,10 +23,60 @@ namespace PT.Web.Controllers
         [HttpGet]
         public List<WorkoutCalenderItem> List()
         {
+            var startDate = DateTime.Now.AddDays(-3);
+            return _db.WorkoutCalenderItem
+                .Include(wci => wci.Workout)
+                .OrderBy(wci => wci.Date)
+                .Where(wci => wci.Date > startDate)
+                .ToList();
+        }
+
+        [HttpGet]
+        [Route("/workoutCalender/history")]
+        public List<WorkoutCalenderItem> History() // TODO: Add pagination
+        {
             return _db.WorkoutCalenderItem
                 .Include(wci => wci.Workout)
                 .OrderBy(wci => wci.Date)
                 .ToList();
         }
+
+        [HttpPost]
+        [Route("/workoutCalender/add")]
+        public async Task Add(AddWorkoutCalenderItem item)
+        {
+            _db.WorkoutCalenderItem.Add(new WorkoutCalenderItem
+            {
+                WorkoutId = item.WorkoutId,
+                Date = item.DateTime.Date,
+            });
+
+            await _db.SaveChangesAsync();
+        }
+
+        [HttpPost]
+        [Route("/workoutCalender/toggleIsCompleted")]
+        public async Task ToggleIsCompleted(ToggleData data)
+        {
+            var workoutCalenderItem =
+                await _db.WorkoutCalenderItem.SingleOrDefaultAsync(t => t.Id == data.WorkoutCalenderItemId) ??
+                throw new Exception($"Couldn't find {nameof(WorkoutCalenderItem)} with id: {data.WorkoutCalenderItemId}");
+
+            workoutCalenderItem.IsCompleted = data.IsCompleted;
+
+            await _db.SaveChangesAsync();
+        }
+    }
+
+    public class AddWorkoutCalenderItem
+    {
+        public DateTimeOffset DateTime { get; set; }
+        public int WorkoutId { get; set; }
+    }
+
+    public class ToggleData
+    {
+        public int WorkoutCalenderItemId { get; set; }
+        public bool IsCompleted { get; set; }
     }
 }
