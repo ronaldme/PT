927a316c1a10ad0b2f9430673e953bd584b0f93a
Ronald <ronaldme@gmail.com>
2020-05-26 18:40:27 +0200

    Improve formik usage / use yup

--- PT.Web/clientapp/src/Components/Workout/AddWorkout.tsx
+++ PT.Web/clientapp/src/Components/Workout/AddWorkout.tsx
@@ -1,10 +1,11 @@
-import React, { useState, Dispatch, SetStateAction, useEffect }  from 'react';
+import React, { useState }  from 'react';
 import axios from 'axios'
-import { useFormik } from 'formik';
+import { Formik } from 'formik';
 import { Button, TextField, Grid, Snackbar } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save';
 import { makeStyles, createStyles, Theme  } from '@material-ui/core/styles';
 import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
+import * as Yup from 'yup';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -22,7 +23,7 @@ type Props = {
   setReload: (reload: boolean) => void;
 }
 
-function AddWorkout  ({ setReload}: Props) {
+function AddWorkout  ({ setReload }: Props) {
   const classes = useStyles();
   const [openSuccess, setOpenSuccess] = useState(false);
   const [openError, setOpenError] = useState(false);
@@ -37,67 +38,74 @@ function AddWorkout  ({ setReload}: Props) {
     setOpenError(false);
   };
 
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-    },
-    onSubmit: values => {
-      axios.post('/workout/add', values)
-      .then(function(){
-        setOpenError(false);
-        setOpenSuccess(true);
-        
-        setReload(true);
-      })
-      .catch(function (error) {
-        setOpenSuccess(false);
-        setOpenError(true);
-      });
-    },
-  });
-
   return (
-    <div className={classes.root}>
-      <form onSubmit={formik.handleSubmit}>
-        <Grid container spacing={1}>
+    <Formik
+      initialValues={{ name: '' }}
+      validationSchema={Yup.object({
+        name: Yup.string()
+          .min(1, 'Workout name must be at least 1 character')
+          .required('Required')
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setSubmitting(false);
 
-          <Grid item xs={2}>
-            <TextField
-              label="Workout name"
-              variant="outlined"
-              size="small"
-              name="name"
-              onChange={formik.handleChange}
-              value={formik.values.name}
-            />
-          </Grid>
+        axios.post('/workout/add', values)
+        .then(function(){
+          setOpenError(false);
+          setOpenSuccess(true);
+          
+          setReload(true);
+        })
+        .catch(function (error) {
+          setOpenSuccess(false);
+          setOpenError(true);
+        });
+      }}
+    >
+      {formik => (
+       <div className={classes.root}>
+       <form onSubmit={formik.handleSubmit}>
+         <Grid container spacing={1}>
 
-          <Grid item xs={2}>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              startIcon={<SaveIcon />}
-            >
-              Add
-            </Button>
-          </Grid>
+           <Grid item xs={2}>
+             <TextField
+               label="Workout name"
+               variant="outlined"
+               size="small"
+               name="name"
+               onChange={formik.handleChange}
+               value={formik.values.name}
+             />
+           </Grid>
 
-        </Grid>
-      </form>
+           <Grid item xs={2}>
+             <Button
+               type="submit"
+               variant="contained"
+               color="primary"
+               startIcon={<SaveIcon />}
+             >
+               Add
+             </Button>
+           </Grid>
 
-      <Snackbar open={openSuccess} autoHideDuration={4000} onClose={handleCloseSuccess}>
-        <Alert onClose={handleCloseSuccess} severity="success">
-          Successfully created new workout!
-        </Alert>
-      </Snackbar>
+         </Grid>
+       </form>
 
-      <Snackbar open={openError} autoHideDuration={4000} onClose={handleCloseError}>
-        <Alert onClose={handleCloseError} severity="error">
-          Cannot create duplicate workouts
-        </Alert>
-      </Snackbar>
-    </div>
+       <Snackbar open={openSuccess} autoHideDuration={4000} onClose={handleCloseSuccess}>
+         <Alert onClose={handleCloseSuccess} severity="success">
+           Successfully created new workout!
+         </Alert>
+       </Snackbar>
+
+       <Snackbar open={openError} autoHideDuration={4000} onClose={handleCloseError}>
+         <Alert onClose={handleCloseError} severity="error">
+           Cannot create duplicate workouts
+         </Alert>
+       </Snackbar>
+     </div> 
+    )}
+    </Formik>
   )
 }
 
